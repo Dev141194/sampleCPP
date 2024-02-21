7eff7dd7064c4540099626a25e6ec117a7931a1c
Dev141194 <109785972+Dev141194@users.noreply.github.com>
2024-02-21 15:01:53 +0530

    Update app.h

--- include/app.h
+++ include/app.h
@@ -1,12 +1,23 @@
 #ifndef APP_H
 #define APP_H
-
 #include <string> // Add this line
 
 class App {
 public:
+
+};
+class App {
+public:
     bool validateUsername(const std::string& username); // Fix function signature
     bool validatePassword(const std::string& password); // Fix function signature
+    App();
+    void run();
+private:
+    void mainMenu();
+    void signup();
+    void login();
+    bool validateUsername(const std::string& username);
+    bool validatePassword(const std::string& password);
 };
 
-#endif /* APP_H */
+#endif // APP_H
